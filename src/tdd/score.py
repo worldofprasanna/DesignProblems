d4528d8cd2b72b2d7f4235badc771cd4f6583108
Prasanna <vprasana@thoughtworks.com>
2016-08-13 22:39:15 +0530

    Empty list should return 0

---
+++ src/tdd/score.py
@@ -0,0 +1,6 @@
+def score(dice):
+    value = 0
+    if len(dice) == 0:
+        return value
+
+
---
+++ src/tdd/test_score.py
@@ -0,0 +1,4 @@
+from .score import score
+def test_score_of_an_empty_list_is_zero():
+    assert score([]) == 0
+
