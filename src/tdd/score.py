ed4bc46687afdb8e5a4a1cd6342994803ea497f5
Prasanna <vprasana@thoughtworks.com>
2016-08-13 22:48:05 +0530

    Score for single roll of 1 and 5

--- src/tdd/score.py
+++ src/tdd/score.py
@@ -3,4 +3,8 @@ def score(dice):
     if len(dice) == 0:
         return value
 
+    if 5 == dice[0]:
+        return 50
+    if 1 == dice[0]:
+        return 100
 
--- src/tdd/test_score.py
+++ src/tdd/test_score.py
@@ -1,4 +1,10 @@
 from .score import score
+
 def test_score_of_an_empty_list_is_zero():
     assert score([]) == 0
 
+def test_score_of_a_single_roll_of_5_is_50():
+    assert 50 == score([5])
+
+def test_score_of_a_single_roll_of_1_is_100():
+    assert 100 == score([1])
